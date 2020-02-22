4d337d920299050c07268b0c4777f8126adcb210
2077946749 <2077946749@qq.com>
2020-02-22 20:45:44 +0800

    unbuilt function

--- findingprimenumber/findingprimenumber.cpp
+++ findingprimenumber/findingprimenumber.cpp
@@ -1,20 +1,39 @@
-﻿// findingprimenumber.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
-//
-
-#include <iostream>
+﻿#include "iostream"
+#include <math.h>
+#define n 100
+int prime[n];
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	int i, j, k;
+    for (int i = 2; i < n; i++)
+	{
+		if (i&1 == 1) //按照按位与来判断奇数和偶数
+			prime[i] = 1;// 奇数存为1
+		else
+			prime[i] = 0;//偶数存为0
+	}
+	prime[2] = 1;//2为素数
+	for (int i = 3; i < n; i += 2)//留下奇数
+	{
+		if (prime[i] == 1)
+		{
+			for (int k = 2; k <= sqrt(i); k++)//对剩下的奇数到根号判断
+				if (i%k == 0)
+					prime[i] = 0;//如果可被整除，排除
+			if (prime[i])//i为素数时，其倍数均排除
+			{
+				for (int j = 2 * i; j < n; j += i)
+					prime[j] = 0;
+			}
+		}
+	}
+	for (int i = 2; i <= n; i++) {
+		if (prime[i] == 1) {
+			cout << "the primenumber is: " << i << endl;
+		}
+	}
+	system("pause");
+	return 0;
 }
-
-// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
-// 调试程序: F5 或调试 >“开始调试”菜单
-
-// 入门使用技巧: 
-//   1. 使用解决方案资源管理器窗口添加/管理文件
-//   2. 使用团队资源管理器窗口连接到源代码管理
-//   3. 使用输出窗口查看生成输出和其他消息
-//   4. 使用错误列表窗口查看错误
-//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
-//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
